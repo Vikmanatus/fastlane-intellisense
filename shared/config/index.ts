80379359fe8bb35417e52c22f59e62e542dc5b5b
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-04-26 13:04:08 +0200

    cleaning architecture

---
+++ shared/config/index.ts
@@ -0,0 +1,18 @@
+import actions_list_completion from "../actions_list.json";
+
+export type ActionListType = {
+  action_name: string;
+  file_path: string;
+  args: null | FastlaneConfigType[]
+};
+
+export type FastlaneConfigType = {
+  key: string;
+  env_name: string | null;
+  description: string | null;
+  default_value: boolean | string | null | any;
+  optional: boolean | null;
+  is_string: boolean | null;
+  data_type: string | null;
+};
+export const actions_list: ActionListType[] = !actions_list_completion.length ? [] : actions_list_completion;
