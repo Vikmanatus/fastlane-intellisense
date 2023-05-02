2a0fa82aa57123822bd2b48f10ade7386c9fa3c7
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-05-02 17:07:27 +0200

    going for provider manager implementation

--- src/client/src/logic/ProvidersManager.ts
+++ src/client/src/logic/ProvidersManager.ts
@@ -1,46 +1,6 @@
-import {
-  DocHoverProvider,
-  ActionDefinitionProvider,
-  VirtualDocumentProvider,
-} from "../providers";
-import Manager from "./Manager";
-import Provider from './Provider';
-
-interface ProviderClassMap {
-  [key: string]: typeof Provider;
-}
-interface ProvidersTypes {
-  docHover: DocHoverProvider;
-  definition: ActionDefinitionProvider;
-  virtualDoc: VirtualDocumentProvider;
-}
-
-enum PROVIDERS {
-  doc = "docHoverProvider",
-  definition = "definitionProvider",
-  virtualDoc = "virtualDocProvider",
-}
-// interface ProviderMap {
-//   [key: string]: Manager;
-// }
-interface OnInitObject {
-  resolved: boolean;
-  promise?: Promise<void>;
-  resolve?: () => void;
-}
-
-interface OnInitMap {
-  [key: string]: OnInitObject;
-}
 class ProvidersManager {
   static _instance?: ProvidersManager | null = null;
-  private readonly providerClasses: ProviderClassMap;
-  constructor() {
-    this.providerClasses = {
-      [PROVIDERS.definition]:ActionDefinitionProvider,
-
-    };
-  }
+  constructor() {}
   public init(): void {
     // Nothing to do for the moment
   }
