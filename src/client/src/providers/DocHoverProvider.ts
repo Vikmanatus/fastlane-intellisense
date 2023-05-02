13b2d9a9b2ca4e496b6a3765a24c427eb000ba6c
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-05-02 19:42:32 +0200

    added initial hover arguments documentation

--- src/client/src/providers/DocHoverProvider.ts
+++ src/client/src/providers/DocHoverProvider.ts
@@ -9,12 +9,29 @@ import {
   languages,
 } from "vscode";
 import Provider from "../logic/Provider";
-import { actions_list } from "@/shared/src/config";
+import { FastlaneConfigType, actions_list } from "@/shared/src/config";
 
 export class DocHoverProvider extends Provider implements HoverProvider {
+  private actionMap: Map<string, MarkdownString>;
+  private argMap: Map<string, FastlaneConfigType>;
+  constructor() {
+    super();
+    this.actionMap = new Map();
+    this.argMap = new Map();
+  }
   public init(): boolean {
     console.log("Initializing DocHoverProvider");
-
+    if (actions_list.length > 0) {
+      for (const action of actions_list) {
+        const markdownString = this.generateActionLink(action.action_name);
+        this.actionMap.set(action.action_name, markdownString);
+        if (action.args) {
+          for (const arg of action.args) {
+            this.argMap.set(arg.key, arg);
+          }
+        }
+      }
+    }
     return true;
   }
   public registerProvider(context: ExtensionContext): void {
@@ -25,6 +42,19 @@ export class DocHoverProvider extends Provider implements HoverProvider {
       )
     );
   }
+  private generateActionLink(actionName: string): MarkdownString {
+    const args = [{ actionName: actionName }];
+    const searchCommandUri = Uri.parse(
+      `command:fastlane-intellisense.openTextDoc?${encodeURIComponent(
+        JSON.stringify(args)
+      )}`
+    );
+    const markdownString = new MarkdownString(
+      `[Open doc for: ${actionName}](${searchCommandUri})`
+    );
+    markdownString.isTrusted = true;
+    return markdownString;
+  }
   provideHover(
     document: TextDocument,
     position: Position,
@@ -32,20 +62,17 @@ export class DocHoverProvider extends Provider implements HoverProvider {
   ) {
     const range = document.getWordRangeAtPosition(position);
     const word = document.getText(range).trim();
-    const args = [{ actionName: word }];
-    const matchWord = actions_list.filter(
-      (element) => element.action_name === word
-    );
-    if (matchWord.length) {
-      const searchCommandUri = Uri.parse(
-        `command:fastlane-intellisense.openTextDoc?${encodeURIComponent(
-          JSON.stringify(args)
-        )}`
-      );
-      const contents = new MarkdownString(
-        `[Open doc for: ${word}](${searchCommandUri})`
-      );
-      contents.isTrusted = true;
+    const matchAction = this.actionMap.get(word);
+    const matchArg = this.argMap.get(word);
+
+    if (matchAction) {
+      return {
+        contents: [matchAction],
+        range,
+      };
+    }
+    if (matchArg && matchArg.description) {
+      const contents = new MarkdownString(matchArg.description);
       return {
         contents: [contents],
         range,
