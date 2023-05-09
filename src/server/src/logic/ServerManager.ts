94ac155ee687fe1edff5473d9fd14bf995dc3427
Viktor Stoyanov <vikman.enterprise@gmail.com>
2023-05-09 16:52:18 +0200

    added required types

--- src/server/src/logic/ServerManager.ts
+++ src/server/src/logic/ServerManager.ts
@@ -1,5 +1,43 @@
-class ServerManager {
+import Server from './Server';
+import ServerConnectionHandlers from './ServerConnectionHandlers';
+import ServerHelper from './ServerHelper';
 
+interface ServerHelperClassesInterface {
+	[key: string]: typeof ServerHelper;
+  }
+  
+  interface ServerHelperTypes {
+	server: Server;
+	serverConnectionHandlers: ServerConnectionHandlers;
+  }
+  
+  interface ServerHelperInterface {
+	[key: string]: ServerHelper;
+  }
+  
+  export enum SERVER_HELPERS {
+	server = "server",
+	serverConnectionHandlers = "serverConnectionHandlers",
+  }
+
+class ServerManager {
+  static _instance?: ServerManager | null = null;
+  private readonly providerClasses: ServerHelperClassesInterface;
+  private readonly providers: ServerHelperInterface;
+  constructor() {
+    this.providerClasses = {
+      [SERVER_HELPERS.server]: Server,
+      [SERVER_HELPERS.serverConnectionHandlers]: ServerConnectionHandlers,
+    };
+    this.providers = {};
+  }
+
+  static getInstance() {
+    if (!ServerManager._instance) {
+      ServerManager._instance = new ServerManager();
+    }
+    return ServerManager._instance;
+  }
 }
 
 export default ServerManager;
